ae2e851c888e5e2bbfa229ea94926ad3f7a87b14
Camily <tctorres@espol.edu.ec>
2024-11-08 20:49:09 -0500

    Creacion de clase padre Entidad

--- src/main/java/Entidad.java
+++ src/main/java/Entidad.java
@@ -1,2 +1,35 @@
-public class Entidad {
+public abstract class Entidad {
+    private int cedula;
+    private String nombre;
+    private int edad;
+
+    public Entidad(int cedula, String nombre, int edad) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
